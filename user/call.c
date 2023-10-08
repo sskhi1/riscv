c3404676c05d919eb89172be5a99ca2dc523c623
sskhi1 <sskhi20@freeuni.edu.ge>
2023-10-08 18:24:18 +0400

    my solution for traps lab exercise 1

--- user/call.c
+++ user/call.c
@@ -13,5 +13,9 @@ int f(int x) {
 
 void main(void) {
   printf("%d %d\n", f(8)+1, 13);
+
+  unsigned int i = 0x646C72;
+  printf("H%x Wo%s\n", 57616, &i);
+  printf("x=%d y=%d\n", 3);
   exit(0);
 }
